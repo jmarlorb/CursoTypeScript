5b53113d2840b1b94f371f6ac2e8b9800a4b0f9a
jmarlorb <jmarlorb@iescarrillo.es>
2024-10-09 14:17:51 +0200

    webpack config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { log } from "console";
+
 console.log("Hello World");
 /**
  * string - Cadenas de caracteres
@@ -95,3 +97,84 @@ muestraTareas(...listaTareas);
 let farrow = (tarea:Tarea,indice:number) => {console.log(`La Tarea ${tarea.nombre} se encuentra en el índice ${indice}`)}
 listaTareas.forEach(farrow);
 listaTareas.forEach(function (elemento:Tarea, indice:number) {console.log(`La Tarea ${elemento.nombre} se encuentra en el índice ${indice}`)});
+
+//funcion callback;
+
+let fsum = function (a:number, b:number){return a+b};
+let fsub = function (a:number, b:number){return a-b};
+
+function operate(x:number, y:number, method:(a:number,b:number)=>number){
+    return method(x,y);
+}
+
+//funciones asincronas
+async function asincrona() {
+    let suma:number = 0;
+    for (let i= 0; i<=100000000; i++){
+        suma+=i;
+    }
+    return suma;
+}
+
+asincrona().then((data:number)=>{console.log(`El resultado de ejecutar asincrona es = ${data}`)});
+
+console.log("Linea posterior a la llamada a la funcion asincrona.");
+
+async function getUniversitiesAsync(pais:string) : Promise <JSON[]> {
+    let index:number = 0;
+    const apiURL:string = "http://universities.hipolabs.com/search?country=";
+
+    //Construimos la URL de la API a consultar concatenando el pais que se quiere filtrar
+    let url:string = `${apiURL}${pais}`;
+    
+    // Con la función fetch accedemos hacemos una petición GET y obtenemos los resultados. 
+    // Usamos await para indicar que hasta que no termine esta instrucción no se ejecuta la siguiente
+    let respuesta:Response = await fetch(url);
+    // Convertimos la respuesta de la petición GET en un archivo JSON
+    let datos:Promise<JSON[]> = await respuesta.json() as Promise<JSON[]>;
+    return datos
+}
+ 
+
+// Llamamos a la función asincrona y mostramos el JSON de las universidades existentes en Spain
+getUniversitiesAsync("Spain").then((data:JSON[])=>{console.log(data[1])});
+
+type university = {
+    domains:String[],
+    alpha_two_code:String,
+    web_pages:String[],
+    name:String,
+
+}
+
+async function getUniversitiesAsynchro(pais:string) : Promise <university[]> {
+    let index:number = 0;
+    const apiURL:string = "http://universities.hipolabs.com/search?country=";
+
+    //Construimos la URL de la API a consultar concatenando el pais que se quiere filtrar
+    let url:string = `${apiURL}${pais}`;
+    
+    // Con la función fetch accedemos hacemos una petición GET y obtenemos los resultados. 
+    // Usamos await para indicar que hasta que no termine esta instrucción no se ejecuta la siguiente
+    let respuesta:Response = await fetch(url);
+    // Convertimos la respuesta de la petición GET en un archivo JSON
+    let datos:Promise<university[]> = await respuesta.json() as Promise<university[]>;
+    return datos
+}
+
+getUniversitiesAsynchro("Spain").then((data:university[])=>{
+    data.forEach((uni:university)=>console.log(`${uni.name}`));
+});
+
+//funciones generadoras
+function* fgeneradora():Generator<Tarea>{
+for (let tarea in listaTareas){
+    yield listaTareas[tarea];
+}
+}
+
+let funciongen = fgeneradora();
+
+funciongen.next(); //Tarea 1
+funciongen.next(); //Tarea 2
+
